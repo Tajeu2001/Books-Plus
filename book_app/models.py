7dcf594db8dadd0b8fd0eb65939440d2e49d27a4
Tajeu2001 <tajeusanta@gmail.com>
2021-07-27 01:21:40 +0300

    Create an app and name it book_app

---
+++ book_app/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from cloudinary.models import CloudinaryField
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = CloudinaryField('image')
+    bio = models.TextField(max_length=500, default="My Bio", blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    location = models.CharField(max_length=60, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def save_profile(self):
+        self.user
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    description = models. Charfield()
+    author = models.CharField(max_length=100)
+    year = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=200)
+    cover = CloudinaryField('image')
+
+    def __str__(self):
+        return self.title
+
---
+++ book_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import re_path,path,include
+from django.contrib.auth import views as auth_views
+from . import views 
+
+
+urlpatterns = [
+]  
