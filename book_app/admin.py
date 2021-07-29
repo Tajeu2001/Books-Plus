78906a2dbee6a0e573a62f3af6448a281481925d
Tajeu2001 <tajeusanta@gmail.com>
2021-07-30 02:27:20 +0300

    Register the models inside the admin.py file

--- book_app/admin.py
+++ book_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile,Book,Rating
+
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Profile)
+admin.site.register(Rating)
